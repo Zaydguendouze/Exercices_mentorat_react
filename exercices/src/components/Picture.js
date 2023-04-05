d77ff9625dd96abad7cae0b51bbdbfb322605f26
Zaydguendouze <z.guendouze@outlook.fr>
2023-04-05 14:46:35 +0200

    test ajout images

---
+++ exercices/src/components/Picture.js
@@ -0,0 +1,11 @@
+import React from "react";
+import images from "./ex_session";
+
+export default function Picture({ imageName, index }) {
+  return (
+    <div>
+      <img className="w-40 mx-auto" src={imageName}></img>
+      <div>{imageName}</div>
+    </div>
+  );
+}
--- exercices/src/components/ex_session.js
+++ exercices/src/components/ex_session.js
@@ -1,16 +1,34 @@
 import "../exercice.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Picture from "./Picture";
 
-const images = ["img1 ", "img2 ", "img3 ", "img4 ", "img5 "];
+// const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];
 
 export default function App() {
+  const [images, setImages] = useState([
+    "img1.jpg ",
+    "img2.jpg ",
+    "img3.jpg ",
+    "img4.jpg ",
+  ]);
   const [isAutocomplete, setIsAutocomplete] = useState(false);
   const [inputText, setInputText] = useState("");
 
-  const [lImages, setLImages] = useState(["img1 "]);
+  const [lImages, setLImages] = useState(["img1.jpg "]);
   const [autocompleteList, setAutocompleteList] = useState(
     images.filter((img) => !lImages.includes(img))
   );
+
+  function ImagesComponent() {
+    return images.map((name, index) => (
+      <Picture
+        imageName={name}
+        index={index}
+        key={index}
+        //   handleRemove={handleRemoveImage}
+      />
+    ));
+  }
 
   const handleInputChange = (e) => {
     const incomingTextInput = e.target.value;
@@ -20,6 +38,7 @@ export default function App() {
       let newAutocompleteList = [...prev];
       autocompleteList.forEach((element) => {
         if (element && element.trim() === incomingTextInput) {
+          console.log("element", element);
           isMatch = true;
           newAutocompleteList = newAutocompleteList.filter(
             (e) => e !== element
@@ -38,6 +57,10 @@ export default function App() {
     // differentes manieres de set un state
     setLImages((prev) => {
       const newLocalImages = [...prev, inputText];
+      console.log("newLocalImages", newLocalImages);
+      console.log("newLocalImages.indexOf()", newLocalImages.indexOf());
+      // console.log("autocompleteList", autocompleteList);
+
       setAutocompleteList(
         images.filter((img) => !newLocalImages.includes(img))
       );
@@ -45,17 +68,60 @@ export default function App() {
     }); // react te donne pas 100% chance de mettre à jour le state avant la prochaine ligne "BATCHING"
   };
 
+  // const handleClick = (e) => {
+  //   // differentes manieres de set un state
+  //   setLImages((prev) => {
+  //     const newLocalImages = [...prev, inputText];
+  //     console.log("newLocalImages", newLocalImages);
+  //     console.log("images", images);
+  //     // console.log("autocompleteList", autocompleteList);
+  //     for (let i = 0; i < newLocalImages.length; i++) {
+  //       for (let j = 0; j < images.length; j++) {
+  //         if (
+  //           newLocalImages[i].trim().toLowerCase() ===
+  //           images[j].trim().toLowerCase()
+  //         ) {
+  //           console.log("OK");
+  //         } else {
+  //           alert("Veuillez ajouter une image existante");
+  //           console.log("NOTOK");
+  //           break;
+  //         }
+  //         console.log("newLocalImages[i]", newLocalImages[i].toLowerCase());
+  //         console.log("images[j]", images[j].toLowerCase());
+  //       }
+  //     }
+  //     setAutocompleteList(
+  //       images.filter((img) => !newLocalImages.includes(img))
+  //     );
+  //     return newLocalImages;
+  //   }); // react te donne pas 100% chance de mettre à jour le state avant la prochaine ligne "BATCHING"
+  // };
+
   useEffect(() => {
     if (inputText.length > 0) {
       setIsAutocomplete(true);
     } else setIsAutocomplete(false);
   }, [inputText]);
 
+  const inputToFocus = useRef(null);
+
+  useEffect(() => {
+    inputToFocus.current.focus();
+    // console.log(inputToFocus);
+  });
+
   return (
-    <div>
+    <div className="container mx-auto">
+      <div className="flex items-center justify-between">
+        <ImagesComponent />
+      </div>
+      <img className="w-40 mx-auto"></img>
+
+      {/* <div>{name}</div> */}
       {lImages.map((img) => img)}
 
-      <input onChange={handleInputChange} />
+      <input ref={inputToFocus} onChange={handleInputChange} />
       <button onClick={handleClick}>ajouter image</button>
 
       {isAutocomplete && (
